1414299428d90ce5335266ab28c46dc5f6f3a579
Jakub Schwenkbeck <Jakub@schwenkbeck.de>
2025-10-09 15:37:34 +0200

    add a finetuning skeleton

--- src/finetune.py
+++ src/finetune.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
 
-def finetune_yolo(saved_model_path: str, dataset_path: str, epochs: int = 10):
+def finetune_model(saved_model_path: str, dataset_path: str, epochs: int = 10):
     model = tf.saved_model.load(saved_model_path)
     
-    # Freeze backbone layers
+    # Freeze an arbritary amount of backbone layers for transfer learning
     for layer in model.layers[:-3]:
         layer.trainable = False
     
-    # Load dataset (you'd need to implement this)
     train_ds = load_custom_dataset(dataset_path)
     
     # Compile and train
@@ -18,3 +17,6 @@ def finetune_yolo(saved_model_path: str, dataset_path: str, epochs: int = 10):
     output_path = f"{saved_model_path}_finetuned"
     tf.saved_model.save(model, output_path)
     return output_path
+
+def load_custom_dataset(dataset_path: str):
+    pass
