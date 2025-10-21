b21463331f7eab958eb3ec6cb6baed08cb6493c2
Gabriele <pattarozzi.gabriele@gmail.com>
2025-10-21 11:12:21 +0200

    refactor: refactored training script

--- train/train.py
+++ train/train.py
@@ -4,19 +4,16 @@ import torch
 # print(torch.version.cuda)
 
 print("PyTorch CUDA: ", torch.cuda.is_available())
-# print(torch.cuda.device_count())
 print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
-# print("Ultralytics CUDA: ", YOLO("yolo11n.pt").device)
 
+data_path = "manipal.yaml"
 
-def train():
-    # Load a model
-    model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+def train(data_path: str):
+    # Load a pretrained model
+    model = YOLO("yolo11n.pt")
 
-    # Train the model using VisDrone dataset
-    # results = model.train(data="VisDrone.yaml", device=0, epochs=100, imgsz=640, batch=16, plots=True)
-    # Train the model using C2A dataset
-    results = model.train(data="c2a-yolo.yaml", device=0, epochs=100, imgsz=640, batch=16, plots=True)
+    # Train the model using a custom dataset
+    results = model.train(data=data_path, device=0, epochs=100, imgsz=640, batch=16, plots=True)
 
 def export():
     best_model = YOLO("runs/detect/train/weights/best.pt")
@@ -25,7 +22,10 @@ def export():
     # best_model.export(format="tflite", imgsz=640, int8=True, data="VisDrone.yaml")
 
 if __name__ == '__main__':
+
     from multiprocessing import freeze_support
-    freeze_support()  # optional, but safe on Windows
-    train()
+    freeze_support()
+
+    train(data_path)
+    
     # export()
